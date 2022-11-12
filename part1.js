6e11baee08254c974d10fbe1e297e6091866880a
demimao <83380862+demimao@users.noreply.github.com>
2022-11-12 12:56:02 -0800

    Update part1.js

--- part1.js
+++ part1.js
@@ -7,7 +7,7 @@ let statistics = {
     rareCars: 2
 };
 
-for (property in statistics) {
+for (const property in statistics) {
     if (property.charAt(0) == "r" || statistics[property] % 2 == 1) {
         console.log(statistics[property]);
     }
