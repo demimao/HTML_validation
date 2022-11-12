3755333dce797ada2f21ed899a00f62eaf445f27
demimao <demimao@users.noreply.github.com>
2022-11-12 22:17:48 +0000

    Prettified Code!

--- part1.js
+++ part1.js
@@ -1,15 +1,14 @@
 let statistics = {
-    redCars: 21,
-    blueCars: 45,
-    greenCars: 12,
-    raceCars: 5,
-    blackCars: 40,
-    rareCars: 2
+  redCars: 21,
+  blueCars: 45,
+  greenCars: 12,
+  raceCars: 5,
+  blackCars: 40,
+  rareCars: 2,
 };
 
 for (const i in statistic) {
-    if (property.charAt(0) == "r" || statistics[property] % 2 == 1) {
-        console.log(statistics[property]);
-    }
-
+  if (property.charAt(0) == "r" || statistics[property] % 2 == 1) {
+    console.log(statistics[property]);
+  }
 }
