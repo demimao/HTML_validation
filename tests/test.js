e8a122a03ed34e8904a36c11a62e78b55edc92a0
demimao <83380862+demimao@users.noreply.github.com>
2022-11-12 12:32:21 -0800

    create error: const property

--- tests/test.js
+++ tests/test.js
@@ -7,7 +7,7 @@ let statistics = {
     rareCars: 2
 };
 
-for (const property in statistics) {
+for (property in statistics) {
     if (property.charAt(0) == "r" || statistics[property] % 2 == 1) {
         console.log(statistics[property]);
     }
